e5648f02d2e6f12b0c79c8e94f8ff80d8f943b62
kimihito <tatsurotamashiro@gmail.com>
2013-01-08 00:22:15 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineMtgRegister::Application.config.secret_token = '85e257946feb56ed07afe3ea70c6bb1285247461c2f9eb57eaef64890bf3923defe6048e4c3401e5390b23d5f719965c0b3d06a06955392e0224b55e25593375'
