360ed1bedb6c0ed66377037571abd2b190c21ee9
kimihito <tatsurotamashiro@gmail.com>
2013-01-17 00:32:57 +0900

    fixed calendar's name and showing id's page selected url

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,33 +1,12 @@
 #coding: utf-8
 class RegisterController < ApplicationController
-	def index
-	end
+  def index
+  end
 
-	def record
-		if params[:name] != ""
-			if current_user.name != params[:name]
-				name = params[:name]
-				current_user.name = name
-				current_user.save!
-
-			end
-		end
-
-
-		if params[:email] != ""
-			if current_user.email != params[:email]
-				email = params[:email]
-				current_user.email = email
-				current_user.save!
-			end
-		end
-		if params[:skype_id] != ""
-			if current_user.skype_id != params[:skype_id]
-				skype_id = params[:skype_id]
-				current_user.skype_id = skype_id
-				current_user.save!
-			end
-		end
-		redirect_to choice_path, :notice => "編集しました"
-	end
+  def record
+    current_user.name=params[:name] if params[:name]!=""
+    current_user.email=params[:email] if params[:email]!=""
+    current_user.skype_id = params[:skype_id] if params[:skype_id] != ""
+    redirect_to choice_path, :notice => "編集しました"
+  end
 end
